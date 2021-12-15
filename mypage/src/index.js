f55e2153b414014417140da57b19a7851f1c4a09
daiki shimayama <d.shimayama123@gmail.com>
2021-12-15 23:16:41 +0900

    基本的なボタンイベントの追加

--- mypage/src/index.js
+++ mypage/src/index.js
@@ -1,26 +1,84 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
-import App from './App';
-import rootReducer from './reducers'
-import { Provider } from 'react-redux'
+import { Provider, connect } from 'react-redux'
 
-const store = createStore(rootReducer);
+//Presentational Components
+class AppComponents extends React.Component {
 
-console.log(store);
+  send(e){
+    this.props.onClick(this.refs.inputText.value);
+  }
 
-ReactDOM.render (
+
+  render() {
+    return (
+      <div>
+        <input type="text" defaultValue="" ref="inputText" /> { /* 入力フォーム */ }
+        <button onClick={this.send.bind(this)}>計算</button>  { /* ボタン */ }
+        <br />
+        {this.props.price}  { /* 表示させる税込の金額 */ }
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    price: state.price
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onClick(price){
+      dispatch(addTax(price));
+    }
+  };
+}
+
+let AppContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AppComponents);
+
+
+
+// ActionCreator
+const ADDTAX = 'ADDTAX';
+function addTax(price) {
+  return {
+    type: ADDTAX,
+    price
+  };
+}
+
+
+// Reducer
+function appReducer(state, action) {
+  switch (action.type) {
+    case 'ADDTAX':
+      return (
+        Object.assign({}, state, {price: action.price * 1.08})
+      );
+    default:
+      return state
+  }
+}
+
+
+//state初期化
+const initialState = {
+  price: ''
+};
+
+//store作成
+const store = createStore(appReducer, initialState);
+
+//レンダリング
+ReactDOM.render(
   <Provider store={store}>
-      <App />
-  </Provider>
-  ,document.getElementById('root')
-)
-
-// 電卓
-// import CalculatorContainer from './containers/CalculatorContainer';
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <CalculatorContainer />
-//   </Provider>,
-//   document.getElementById('root')
-// );
+    <AppContainer />
+  </Provider>,
+  document.getElementById('root')
+);
