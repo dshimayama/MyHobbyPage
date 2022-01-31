e39fbb2e85473c0b1908ac737a0f6bbf09f51acb
daiki shimayama <d.shimayama123@gmail.com>
2022-01-31 17:33:01 +0900

    firebase init

--- mypage/src/index.js
+++ mypage/src/index.js
@@ -1,7 +1,12 @@
+// React関連
+import './index.css'
+import App from './App';
 import React from 'react'
 import ReactDOM from 'react-dom'
+// Redux関連
 import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
+
 
 //Presentational Components
 class AppComponents extends React.Component {
@@ -23,26 +28,29 @@ class AppComponents extends React.Component {
   }
 }
 
+// Presentational Componentsに渡すstateとしてpriceを指定
 function mapStateToProps(state) {
   return {
     price: state.price
   };
 }
 
+// Presentational Componentsに渡す関数としてonClickを指定
 function mapDispatchToProps(dispatch) {
   return {
     onClick(price){
-      dispatch(addTax(price));
+      dispatch(addTax(price));  // dispatch(関数)でreducerが実行
     }
   };
 }
 
+// connectでreactとreduxを繋げている
+// stateを返す関数と変更する関数を指定
+// classの名前を指定
 let AppContainer = connect(
   mapStateToProps,
   mapDispatchToProps
 )(AppComponents);
-
-
 
 // ActionCreator
 const ADDTAX = 'ADDTAX';
@@ -53,8 +61,9 @@ function addTax(price) {
   };
 }
 
-
 // Reducer
+// stateとactionを引数に指定する
+// Actionのpriceを1.08倍してStateにマージし新しいStateとして返す
 function appReducer(state, action) {
   switch (action.type) {
     case 'ADDTAX':
@@ -66,13 +75,14 @@ function appReducer(state, action) {
   }
 }
 
-
 //state初期化
 const initialState = {
   price: ''
 };
 
-//store作成
+// store作成
+// reducerとState初期値でStoreを作成
+// それをviewに渡してreactでstoreを使う
 const store = createStore(appReducer, initialState);
 
 //レンダリング
